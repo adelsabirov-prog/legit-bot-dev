6540b24110fce2b311d7fa0ee1ce62f4191befdc
adelsabirov-prog <adelsabirov@gmail.com>
2026-08-11 15:51:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ try:
     OCR3=RapidOCR()
     logging.info("OCR3 RapidOCR ready")
 except Exception:
-    logging.info("OCR3 import failed")
+    logging.info("OCR3 import failed: %r,e")
 
 def ocr3_read(b64):
     if OCR3 is None: return None
